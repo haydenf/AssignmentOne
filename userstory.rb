3abdb1e56f418c2b82d04a44eca7834356ce1e74
haydenf <hayden.fuller@gmail.com>
2019-04-23 14:20:36 +1000

    changing story to be more intouch with what we are implementing

--- userstory.rb
+++ userstory.rb
@@ -1,6 +1,7 @@
 Userstory
 Enter main menu
 enter the player names
+information on what adlibs is 
 select story
 each user gets to chose a word to fill in the blank
 after every blank has been filled, prints out entire story for user to read 
@@ -10,7 +11,7 @@ ability to exit app or continue onto another story
 Psuedo
 
 1. Open up show MadLibs as a title, press enter to start
-2. Get user to select how many players are playing maximum is 4.
+2. Information on adlibs and how to play
 3. Get user to enter name for each user playing and store this into an array
 4. After user has entered names, get them to select a story using 1/2/3/4 keys 
 story is based on a topic. 
