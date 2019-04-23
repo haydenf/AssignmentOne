d0795b8d16a0bf520455cc537176f9fc767a0e49
haydenf <hayden.fuller@gmail.com>
2019-04-23 10:58:23 +1000

    user story for us to follow

---
+++ userstory.rb
@@ -0,0 +1,23 @@
+Userstory
+Enter main menu
+choose amount of players
+enter the player names
+select story
+each user gets to chose a word to fill in the blank
+after every blank has been filled, prints out entire story for user to read 
+ability to exit app or continue onto another story
+
+
+Psuedo
+
+1. Open up show MadLibs as a title, press enter to start
+2. Get user to select how many players are playing maximum is 4.
+3. Get user to enter name for each user playing and store this into an array
+4. After user has entered names, get them to select a story using 1/2/3/4 keys 
+story is based on a topic. 
+5. story is selected then it will go through and fill all the blanks, will
+put out username : enter a noun. user : enter a verb and push all these
+into an empty array
+6. after all blanks have been filled, grab the elements in the 
+array and put them into the blanks of the story.
+7. give user option to quit
