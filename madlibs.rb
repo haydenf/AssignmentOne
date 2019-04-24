d279a0fdabd70a2cd797f0904fead559c8520cc7
Daniel Robertson <robertsondaniel542@gmail.com>
2019-04-24 11:29:10 +1000

    updated to case statements for users to select story

--- madlibs.rb
+++ madlibs.rb
@@ -1,6 +1,7 @@
 #enter a name
 require_relative 'feature3.rb'
 require_relative 'feature3_2.rb'
+require 'colorize'
 
 class Name
 
@@ -23,17 +24,39 @@ class Name
 end
 Name.new.user_name
 
-class  MadStories
-    def madlib_story
-        while true
-            puts "please enter 1 for A Day At The Zoo or enter 2 for The Fun park"
-            entry_input = gets.chomp.to_i
-            break if entry_input.empty?
+class ChoiceMenu
 
-        end
+    def users_pick
+        puts "User, pick 1 to play Zoo story, pick 2 to play
+        Jungle story or 0 to exit"
     end 
 end
 
+user_choice = ChoiceMenu.new
+zoo_story = ZooStory.new
+jungle_story = JungleStory.new
+
+exit = fasle
+while exit ==fasle
+
+    user_choice.users_pick
+    user_number = gets.chomp.to_i
+
+    case user_number
+    when 1
+
+        zoo_story.user_inputs
+        zoo_story.full_story
+    when 2
+        jungle_story.user_inputs
+        jungle_story.full_story
+    when 0
+        puts "Thanks for playing adlibs"
+        exit = true
+    else
+        puts "invalid key buddy, try again"
+    end
+end 
 def method_name
     puts "hello"
 end
@@ -41,4 +64,7 @@ end
 
 MadStories.new.madlib_story
 
+# if entry_input = 1
+#     ZooStory.new.user_inputs
+#     ZooStory.new.full_story
 
